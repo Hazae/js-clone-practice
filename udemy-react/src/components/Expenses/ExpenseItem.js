98f2b5cf11e42af41b10d84a5036fbdf38c7a54a
yena.kwon <yena.kwon@ptbwa.com>
2022-06-02 18:43:22 +0900

    feat #5: useState hook 확인

---
+++ udemy-react/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+// props를 통해 App.js에 있는 expense 객체의 속성 값들을 가져올 수 있음.
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
+  const clickHandler = () => {
+    setTitle('Updated!!');
+    console.log(title);
+  };
+  return (
+    // Card  컴포넌트는 사용자 지정 컴포넌트이기 때문에 리액트에 내장된 클래스 네임 속성을 지원하지 않음(사용자가 만들지 않았기 때문).
+    // 따라서 해당 컴포넌트 파일 내에서 추가적인 처리가 필요함.
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
